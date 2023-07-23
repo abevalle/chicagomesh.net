1cea9caf33150ee43106132ac17f847c77bbef42
abe valle <abe@valle.us>
2023-07-22 23:02:41 -0500

    init commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <img src="logo.png"/>
+      <h3>Future Home of Chicago Mesh Network</h3>
+      <h4>hello@chicagomes.net</h4>
+    </div>
+  )
+}
